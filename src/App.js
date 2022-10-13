1e2712ca7f24a9c3d3e44c7b6d45465561887fc3
sadnanrakib <sadnanrakib02@gmail.com>
2022-10-13 23:45:19 +0600

    add

--- src/App.js
+++ src/App.js
@@ -1,17 +1,68 @@
-
 import './App.css';
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth'
 import app from './firebase.init';
+import { useState } from 'react';
 
-const auth = getAuth(app)
+const auth = getAuth(app);
+
 function App() {
-  const provider = new GoogleAuthProvider();
-  const handleSingIn =()=>{
-    console.log("sing in")
+  const [user, setUser] = useState({})
+  const googleProvider = new GoogleAuthProvider();
+  const githubProvider = new GithubAuthProvider();
+
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, googleProvider)
+      .then(result => {
+        const user = result.user;
+        setUser(user);
+        console.log(user);
+      })
+      .catch(error => {
+        console.error('error: ', error);
+      })
   }
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUser({});
+      })
+      .catch(() => {
+        setUser({})
+      })
+  }
+
+  const handleGithubSignIn= () =>{
+    signInWithPopup(auth, githubProvider)
+    .then( result => {
+      const user = result.user;
+      setUser(user);
+      console.log(user);
+    })
+    .catch( error =>{
+      console.error ('error: ', error)
+    })
+  }
+
   return (
     <div className="App">
-      <button onClick={handleSingIn}>Google Sing In</button>
+      {/* condition ? true: false */}
+
+      {
+        user.uid ?
+          <button onClick={handleSignOut}>Sign Out</button>
+          :
+          <>
+            <button onClick={handleGoogleSignIn}>Google Sing In</button>
+            <button onClick={handleGithubSignIn}>Github Sign IN</button>
+          </>
+
+      }
+      {user.uid && <div>
+        <h3>User name: {user.displayName}</h3>
+        <p>Email address: {user.email}</p>
+        <img src={user.photoURL} alt="" />
+      </div>}
     </div>
   );
 }
