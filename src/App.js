9cc2162207952d621f5a86f98a93f08c0330678a
sadnanrakib <sadnanrakib02@gmail.com>
2022-10-13 22:51:21 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import app from './firebase.init';
+
+const auth = getAuth(app)
 function App() {
+  const provider = new GoogleAuthProvider();
+  const handleSingIn =()=>{
+    console.log("sing in")
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleSingIn}>Google Sing In</button>
     </div>
   );
 }
---
+++ src/firebase.init.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCp7oL8mrp1mwxpqHoRPMWSaNUIp3XJiPA",
+  authDomain: "simple-firebase-authenti-84e87.firebaseapp.com",
+  projectId: "simple-firebase-authenti-84e87",
+  storageBucket: "simple-firebase-authenti-84e87.appspot.com",
+  messagingSenderId: "493118419496",
+  appId: "1:493118419496:web:cf3a804e00808107fbfb1d",
+  measurementId: "G-EVQN372GL4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+export default app;
